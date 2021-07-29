edd4675301b6cd6ea6e9016a1c57c6818cd6c9f7
nuthapongl <nuthapong.lert@gmail.com>
2021-07-29 17:02:04 +0700

    del 2 comment

--- one_tof_gate.ino
+++ one_tof_gate.ino
@@ -38,17 +38,9 @@ void setup()
     while (1);
   }
 
-  // Use long distance mode and allow up to 50000 us (50 ms) for a measurement.
-  // You can change these settings to adjust the performance of the sensor, but
-  // the minimum timing budget is 20 ms for short distance mode and 33 ms for
-  // medium and long distance modes. See the VL53L1X datasheet for more
-  // information on range and timing limits.
   sensor.setDistanceMode(VL53L1X::Long);
   sensor.setMeasurementTimingBudget(15000);
 
-  // Start continuous readings at a rate of one measurement every 50 ms (the
-  // inter-measurement period). This period should be at least as long as the
-  // timing budget.
   sensor.startContinuous(20);
 
   delay(500);
